608519ae97b5f2698f364e20b8e0c5f0dc045eeb
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2026-03-25 14:51:36 +0100

    feat: Add responsive header navigation with interactive menu functionality and initial hero section styling.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
+"use strict";
 import "./styles/main.scss";
+
+const selectElement = (elementSelector) => {
+  const element = document.querySelector(elementSelector);
+
+  if (!element) {
+    console.error(
+      `Element with selector ${elementSelector} does not exist. Check for any spelling mistakes`,
+    );
+    return;
+  }
+  return element;
+};
+
+// NAVBAR LOGIC
+const openBtn = selectElement("#openMenu");
+const closeBtn = selectElement("#closeMenu");
+const navMenu = selectElement(".nav");
+
+openBtn?.addEventListener("click", () => {
+  openBtn.setAttribute("aria-expanded", "true");
+  closeBtn.setAttribute("aria-expanded", "true");
+  navMenu.classList.remove("close-nav");
+});
+
+closeBtn?.addEventListener("click", () => {
+  openBtn.setAttribute("aria-expanded", "false");
+  closeBtn.setAttribute("aria-expanded", "false");
+  navMenu.classList.add("close-nav");
+});
